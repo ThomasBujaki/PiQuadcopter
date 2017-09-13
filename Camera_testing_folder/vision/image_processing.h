aeb4f8261b39b8a9d8cb2c3ed250f303d9b237cf
Thomas Bujaki <ofbujak@gmail.com>
2017-09-13 10:07:45 -0400

    greyscale and sobel operatons added to imageprocessing.c

--- Camera_testing_folder/vision/image_processing.h
+++ Camera_testing_folder/vision/image_processing.h
@@ -3,15 +3,16 @@
 #pragma once
 
 typedef struct rgb_pixel {
-        uint_fast8_t red;
-        uint_fast8_t green;
-        uint_fast8_t blue;
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
 } rgb_pixel;
 
 typedef struct gs_pixel {
-	uint_fast16_t grey;
+	uint16_t pxl;
 } gs_pixel;
 
 
-int make_greyscale (rgb_pixel* color_buffer, gs_pixel* greyscale_buffer; int image_width, int image_height);
+int make_greyscale (rgb_pixel* color_buffer, gs_pixel* greyscale_buffer, int image_width, int image_height);
 
+int
