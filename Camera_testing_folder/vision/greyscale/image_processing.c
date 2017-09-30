5938e174298fd0bc22f7f8f3c72cd1c7b5115410
Thomas Bujaki <ofbujak@gmail.com>
2017-09-30 15:38:21 -0400

    commit

--- Camera_testing_folder/vision/greyscale/image_processing.c
+++ Camera_testing_folder/vision/greyscale/image_processing.c
@@ -1,10 +1,13 @@
 #include "image_processing.h"
+#include "greyscale.h"
 
-
-*canny image_processing(rgb_pixel* raw_image) {
-  // Step 1: convert to grey scale
-  // Step 2: guasian smoothing
-  // Step 3: sobel
-  // Step 4: canny
-  // Step 5: profit.
+gs_pixel* image_processing(rgb_pixel* raw_image) {
+	// Step 1: convert to grey scale
+	gs_pixel *gs_image;
+	make_greyscake(raw_image, gs_image, image_height,image_width);
+	// Step 2: guasian smoothing
+	// Step 3: sobel
+	// Step 4: canny
+	// Step 5: profit.
+	return gs_address;
 }
--- Camera_testing_folder/vision/image_processing.h
+++ Camera_testing_folder/vision/image_processing.h
@@ -15,4 +15,3 @@ typedef struct gs_pixel {
 
 int make_greyscale (rgb_pixel* color_buffer, gs_pixel* greyscale_buffer, int image_width, int image_height);
 
-int
