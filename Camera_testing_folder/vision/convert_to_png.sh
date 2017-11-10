31ebeaa952882e421f011e881eda2476d83c3121
Spencer Whyte <spencerwhyte@gmail.com>
2017-11-09 19:38:44 -0500

    Added ability to display 16-bit grayscale images

---
+++ Camera_testing_folder/vision/convert_to_png.sh
@@ -0,0 +1,10 @@
+gcc -lm -lpng -o convert_to_png.out convert_to_png.c
+chmod +x convert_to_png.out
+./convert_to_png.out $1
+
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+if [ "$UNAME" == "linux" ]; then
+	xdg-open out.png
+else
+	open out.png
+fi
