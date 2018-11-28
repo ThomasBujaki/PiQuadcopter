56c71315d3c3b2adea4d1abfc98dc765b0f5b113
Spencer Whyte <spencerwhyte@gmail.com>
2018-11-28 18:12:31 -0500

    Removed Camera_testing_folder that we accidentally added

--- Camera_testing_folder/vision/memory_management.c
+++
@@ -1,7 +0,0 @@
-#include "memory_management.h"
-
-void free_all(int *memory_ptrs, int size_memory_ptr){
-	for (int i = 0; i < size_memory_ptr; i++){
-		free(memory_ptrs[i]);
-	}
-}
--- Camera_testing_folder/vision/memory_management.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void free_all(int *memory_ptrs, int size_memory_ptr);
