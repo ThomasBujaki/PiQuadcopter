226ff4698400283960378184f3dc23dba1599506
Spencer Whyte <spencerwhyte@gmail.com>
2018-11-28 20:14:01 -0500

    First step in moving to c++. Started using the c++ compiler and fixed all of the build errors.

--- ComputerVision/matrix.c
+++ ComputerVision/matrix.c
@@ -1,7 +1,7 @@
 #include "matrix.h"
 
 void init_matrix(matrix* m, uint32_t width, uint32_t height) {
-  m->data = calloc(width*height, sizeof(float));
+  m->data = (float *)calloc(width*height, sizeof(float));
   m->width = width;
   m->height = height;
 }
